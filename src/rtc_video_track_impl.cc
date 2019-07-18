11fe417cfd6ae95b3dadc1f2d7b36879928b7dfc
CloudWebRTC <duanweiwei1982@gmail.com>
2019-07-18 17:22:47 +0800

    Fixed compiler error on linux.

--- src/rtc_video_track_impl.cc
+++ src/rtc_video_track_impl.cc
@@ -9,8 +9,8 @@ VideoTrackImpl::VideoTrackImpl(
     rtc::scoped_refptr<webrtc::VideoTrackInterface> rtc_track)
     : rtc_track_(rtc_track),
       video_sink_(new RefCountedObject<VideoSinkAdapter>(rtc_track)) {
-  snprintf(id_, kMaxStringLength, rtc_track_->id().c_str());
-  snprintf(kind_, kMaxStringLength, rtc_track_->kind().c_str());
+  strncpy(id_, rtc_track_->id().c_str(), kMaxStringLength);
+  strncpy(kind_, rtc_track_->kind().c_str(), kMaxStringLength);
   RTC_LOG(INFO) << __FUNCTION__ << ": ctor ";
 }
 
