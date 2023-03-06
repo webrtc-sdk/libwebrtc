4b82433572fdf0bc094f793bcc2ef4bb2c24f8fd
Mark Dicks <117034609+markdicks@users.noreply.github.com>
2023-03-06 03:22:37 +0200

    adding doc strings and reverting code change

--- src/libwebrtc.cc
+++ src/libwebrtc.cc
@@ -1,9 +1,5 @@
-/**
-
-@file libwebrtc.h
-@brief Contains the implementation of the LibWebRTC class.
-*/
 #include "libwebrtc.h"
+
 #include "api/scoped_refptr.h"
 #include "rtc_base/ssl_adapter.h"
 #include "rtc_base/thread.h"
@@ -12,81 +8,80 @@
 
 namespace libwebrtc {
 
+// Initialize static variable g_is_initialized to false.
 static bool g_is_initialized = false;
-std::unique_ptrrtc::Thread worker_thread;
-std::unique_ptrrtc::Thread signaling_thread;
-std::unique_ptrrtc::Thread network_thread;
+// Thread pointers for different thread types.
+std::unique_ptr<rtc::Thread> worker_thread;
+std::unique_ptr<rtc::Thread> signaling_thread;
+std::unique_ptr<rtc::Thread> network_thread;
 
-/**
-
-@brief Initializes the required threads and SSL for the LibWebRTC library.
-
-@return true if initialization was successful, false otherwise.
-*/
+// Initializes SSL and the required threads, if not initialized.
 bool LibWebRTC::Initialize() {
-if (!g_is_initialized) {
-rtc::InitializeSSL();
-g_is_initialized = true;
+  if (!g_is_initialized) {
+    rtc::InitializeSSL();
+    g_is_initialized = true;
 
-if (worker_thread.get() == nullptr) {
-worker_thread = rtc::Thread::Create();
-worker_thread->SetName("worker_thread", nullptr);
-RTC_CHECK(worker_thread->Start()) << "Failed to start thread";
+    // Creates and starts the worker thread.
+    if (worker_thread.get() == nullptr) {
+      worker_thread = rtc::Thread::Create();
+      worker_thread->SetName("worker_thread", nullptr);
+      RTC_CHECK(worker_thread->Start()) << "Failed to start thread";
+    }
+
+    // Creates and starts the signaling thread.
+    if (signaling_thread.get() == nullptr) {
+      signaling_thread = rtc::Thread::Create();
+      signaling_thread->SetName("signaling_thread", NULL);
+      RTC_CHECK(signaling_thread->Start()) << "Failed to start thread";
+    }
+
+    // Creates and starts the network thread.
+    if (network_thread.get() == nullptr) {
+      network_thread = rtc::Thread::CreateWithSocketServer();
+      network_thread->SetName("network_thread", nullptr);
+      RTC_CHECK(network_thread->Start()) << "Failed to start thread";
+    }
+  }
+  return g_is_initialized;
 }
 
-if (signaling_thread.get() == nullptr) {
-signaling_thread = rtc::Thread::Create();
-signaling_thread->SetName("signaling_thread", NULL);
-RTC_CHECK(signaling_thread->Start()) << "Failed to start thread";
-}
-
-if (network_thread.get() == nullptr) {
-network_thread = rtc::Thread::CreateWithSocketServer();
-network_thread->SetName("network_thread", nullptr);
-RTC_CHECK(network_thread->Start()) << "Failed to start thread";
-}
-}
-return g_is_initialized;
-}
-
-/**
-
-@brief Terminates the threads and SSL used by the LibWebRTC library.
-*/
+// Stops and cleans up the threads and SSL.
 void LibWebRTC::Terminate() {
-rtc::ThreadManager::Instance()->SetCurrentThread(NULL);
-rtc::CleanupSSL();
-if (worker_thread.get() != nullptr) {
-worker_thread->Stop();
-worker_thread.reset(nullptr);
+  rtc::ThreadManager::Instance()->SetCurrentThread(NULL);
+  rtc::CleanupSSL();
+
+  // Stops and resets the worker thread.
+  if (worker_thread.get() != nullptr) {
+    worker_thread->Stop();
+    worker_thread.reset(nullptr);
+  }
+
+  // Stops and resets the signaling thread.
+  if (signaling_thread.get() != nullptr) {
+    signaling_thread->Stop();
+    signaling_thread.reset(nullptr);
+  }
+
+  // Stops and resets the network thread.
+  if (network_thread.get() != nullptr) {
+    network_thread->Stop();
+    network_thread.reset(nullptr);
+  }
+
+  // Resets the static variable g_is_initialized to false.
+  g_is_initialized = false;
 }
 
-if (signaling_thread.get() != nullptr) {
-signaling_thread->Stop();
-signaling_thread.reset(nullptr);
-}
-
-if (network_thread.get() != nullptr) {
-network_thread->Stop();
-network_thread.reset(nullptr);
-}
-
-g_is_initialized = false;
-}
-
-/**
-
-@brief Creates an RTCPeerConnectionFactory object and initializes it.
-@return a scoped_refptr<RTCPeerConnectionFactory> object.
-*/
+// Creates and returns an instance of RTCPeerConnectionFactory.
 scoped_refptr<RTCPeerConnectionFactory>
 LibWebRTC::CreateRTCPeerConnectionFactory() {
-scoped_refptr<RTCPeerConnectionFactory> rtc_peerconnection_factory =
-scoped_refptr<RTCPeerConnectionFactory>(
-new RefCountedObject<RTCPeerConnectionFactoryImpl>(
-worker_thread.get(), signaling_thread.get(),
-network_thread.get()));
-rtc_peerconnection_factory->Initialize();
-return rtc_peerconnection_factory;
+  scoped_refptr<RTCPeerConnectionFactory> rtc_peerconnection_factory =
+      scoped_refptr<RTCPeerConnectionFactory>(
+          new RefCountedObject<RTCPeerConnectionFactoryImpl>(
+              worker_thread.get(), signaling_thread.get(),
+              network_thread.get()));
+  rtc_peerconnection_factory->Initialize();
+  return rtc_peerconnection_factory;
 }
-} // namespace libwebrtc
+
+}  // namespace libwebrtc
