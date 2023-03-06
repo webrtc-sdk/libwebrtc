d0fac58c796f37ac5f34c44ed22852449da85fa4
Mark Dicks <117034609+markdicks@users.noreply.github.com>
2023-03-06 02:58:41 +0200

    add doc string format.sh

--- format.sh
+++ format.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+# This script finds all files with .cc or .h extensions in the current directory 
+# and its subdirectories, and applies the clang-format tool to format them according
+# to the style defined in a configuration file named ".clang-format" located in the 
+# root directory of the project. The "-i" option overwrites the original files with 
+# the formatted version.
 find . -name "*.cc" -o -name "*.h" | xargs clang-format -style=file -i
