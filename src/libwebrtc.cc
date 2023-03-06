5a430abb088fdf22d5a9bc82db831e379ef7ea33
Mark Dicks <117034609+markdicks@users.noreply.github.com>
2023-03-06 05:58:19 +0200

    I added doc strings to 3 files (#67)
    * add doc string format.sh
    
    * add doc strings to helper.cc
    
    * added doc strings here as well
    
    * adding doc strings and reverting code change
    
    * add doc strings to helper.h
    
    * deep doc strings added to libwebrtc.h
    
    * Deep doc strings for rtc_audio_device.h
    
    * details doc strings for rtc_audio_frame.h

--- src/libwebrtc.cc
+++ src/libwebrtc.cc
@@ -8,28 +8,34 @@
 
 namespace libwebrtc {
 
+// Initialize static variable g_is_initialized to false.
 static bool g_is_initialized = false;
+// Thread pointers for different thread types.
 std::unique_ptr<rtc::Thread> worker_thread;
 std::unique_ptr<rtc::Thread> signaling_thread;
 std::unique_ptr<rtc::Thread> network_thread;
 
+// Initializes SSL and the required threads, if not initialized.
 bool LibWebRTC::Initialize() {
   if (!g_is_initialized) {
     rtc::InitializeSSL();
     g_is_initialized = true;
 
+    // Creates and starts the worker thread.
     if (worker_thread.get() == nullptr) {
       worker_thread = rtc::Thread::Create();
       worker_thread->SetName("worker_thread", nullptr);
       RTC_CHECK(worker_thread->Start()) << "Failed to start thread";
     }
 
+    // Creates and starts the signaling thread.
     if (signaling_thread.get() == nullptr) {
       signaling_thread = rtc::Thread::Create();
       signaling_thread->SetName("signaling_thread", NULL);
       RTC_CHECK(signaling_thread->Start()) << "Failed to start thread";
     }
 
+    // Creates and starts the network thread.
     if (network_thread.get() == nullptr) {
       network_thread = rtc::Thread::CreateWithSocketServer();
       network_thread->SetName("network_thread", nullptr);
@@ -39,28 +45,34 @@ bool LibWebRTC::Initialize() {
   return g_is_initialized;
 }
 
+// Stops and cleans up the threads and SSL.
 void LibWebRTC::Terminate() {
   rtc::ThreadManager::Instance()->SetCurrentThread(NULL);
   rtc::CleanupSSL();
 
+  // Stops and resets the worker thread.
   if (worker_thread.get() != nullptr) {
     worker_thread->Stop();
     worker_thread.reset(nullptr);
   }
 
+  // Stops and resets the signaling thread.
   if (signaling_thread.get() != nullptr) {
     signaling_thread->Stop();
     signaling_thread.reset(nullptr);
   }
 
+  // Stops and resets the network thread.
   if (network_thread.get() != nullptr) {
     network_thread->Stop();
     network_thread.reset(nullptr);
   }
 
+  // Resets the static variable g_is_initialized to false.
   g_is_initialized = false;
 }
 
+// Creates and returns an instance of RTCPeerConnectionFactory.
 scoped_refptr<RTCPeerConnectionFactory>
 LibWebRTC::CreateRTCPeerConnectionFactory() {
   scoped_refptr<RTCPeerConnectionFactory> rtc_peerconnection_factory =
