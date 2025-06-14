182ae4f065c9d167ec53a46ea75599096ca859ec
cloudwebrtc <duanweiwei1982@gmail.com>
2025-06-14 16:57:25 +0800

    fix.

--- include/rtc_media_track.h
+++ include/rtc_media_track.h
@@ -10,6 +10,8 @@ class AudioTrackSink {
   virtual void OnData(const void* audio_data, int bits_per_sample,
                       int sample_rate, size_t number_of_channels,
                       size_t number_of_frames) = 0;
+ protected:
+  virtual ~AudioTrackSink() {}
 };
 
 /*Media Track interface*/
