21962602fcd6e7f16e9a92411abdb933b313233f
cloudwebrtc <duanweiwei1982@gmail.com>
2025-11-21 18:29:47 +0800

    fix: Add RTCAudioOptions and enable SW 3A for Linux.

--- include/rtc_peerconnection_factory.h
+++ include/rtc_peerconnection_factory.h
@@ -43,7 +43,8 @@ class RTCPeerConnectionFactory : public RefCountInterface {
   virtual scoped_refptr<RTCAudioSource> CreateAudioSource(
       const string audio_source_label,
       RTCAudioSource::SourceType source_type =
-          RTCAudioSource::SourceType::kMicrophone) = 0;
+          RTCAudioSource::SourceType::kMicrophone,
+        RTCAudioOptions options = RTCAudioOptions()) = 0;
 
   virtual scoped_refptr<RTCVideoSource> CreateVideoSource(
       scoped_refptr<RTCVideoCapturer> capturer, const string video_source_label,
