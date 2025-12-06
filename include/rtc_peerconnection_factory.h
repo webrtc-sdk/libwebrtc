d3a05b6effa9df88a2df8395d4a746636d3b4c45
Dronnis Marvel <dronnis@gmail.com>
2025-12-06 12:11:11 +0300

    Update rtc_peerconnection_factory

--- include/rtc_peerconnection_factory.h
+++ include/rtc_peerconnection_factory.h
@@ -11,6 +11,8 @@
 #include "rtc_mediaconstraints.h"
 #include "rtc_video_device.h"
 #include "rtc_video_source.h"
+#include "rtc_dummy_video_capturer.h"
+#include "rtc_dummy_audio_source.h"
 
 namespace libwebrtc {
 
@@ -22,7 +24,7 @@ class RTCRtpCapabilities;
 
 class RTCPeerConnectionFactory : public RefCountInterface {
  public:
-  virtual bool Initialize() = 0;
+  virtual bool Initialize(bool use_dummy_audio = false) = 0;
 
   virtual bool Terminate() = 0;
 
@@ -44,6 +46,12 @@ class RTCPeerConnectionFactory : public RefCountInterface {
       const string audio_source_label,
       RTCAudioSource::SourceType source_type =
           RTCAudioSource::SourceType::kMicrophone) = 0;
+          
+  virtual scoped_refptr<RTCDummyAudioSource> CreateDummyAudioSource(
+      const string audio_source_label,
+      int sample_rate_hz = 16000,
+      uint32_t num_channels = 2
+  ) = 0;
 
   virtual scoped_refptr<RTCVideoSource> CreateVideoSource(
       scoped_refptr<RTCVideoCapturer> capturer, const string video_source_label,
@@ -54,8 +62,16 @@ class RTCPeerConnectionFactory : public RefCountInterface {
       const string video_source_label,
       scoped_refptr<RTCMediaConstraints> constraints) = 0;
 #endif
+  virtual scoped_refptr<RTCDummyVideoCapturer> CreateDummyVideoCapturer(
+      uint32_t fps, uint32_t width, uint32_t height) = 0;
+  virtual scoped_refptr<RTCVideoSource> CreateDummyVideoSource(
+      scoped_refptr<RTCDummyVideoCapturer> capturer, const string video_source_label) = 0;
+
   virtual scoped_refptr<RTCAudioTrack> CreateAudioTrack(
       scoped_refptr<RTCAudioSource> source, const string track_id) = 0;
+
+  virtual scoped_refptr<RTCAudioTrack> CreateAudioTrack(
+      scoped_refptr<RTCDummyAudioSource> source, const string track_id) = 0;
 
   virtual scoped_refptr<RTCVideoTrack> CreateVideoTrack(
       scoped_refptr<RTCVideoSource> source, const string track_id) = 0;
@@ -68,6 +84,8 @@ class RTCPeerConnectionFactory : public RefCountInterface {
 
   virtual scoped_refptr<RTCRtpCapabilities> GetRtpReceiverCapabilities(
       RTCMediaType media_type) = 0;
+
+  virtual bool GetUseDummyAudio() = 0;
 };
 
 }  // namespace libwebrtc
