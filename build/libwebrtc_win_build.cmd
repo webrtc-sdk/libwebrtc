eda4583ace2e0f5f73ab1c4853cbb5c3fc79d8f6
cloudwebrtc <duanweiwei1982@gmail.com>
2026-05-25 10:29:30 +0800

    update.

--- build/libwebrtc_win_build.cmd
+++ build/libwebrtc_win_build.cmd
@@ -42,7 +42,7 @@ set DEPOT_TOOLS_WIN_TOOLCHAIN=0
 set GYP_GENERATORS=ninja,msvs-ninja
 set GYP_MSVS_VERSION=2022
 set OUTPUT_DIR=src\out-!arch!-!profile!
-set ARTIFACTS_DIR=%cd%\win-!arch!-!profile!
+set ARTIFACTS_DIR=%cd%\libwebrtc-!arch!-!profile!
 set vs2019_install=C:\Program Files\Microsoft Visual Studio\2022\Enterprise
 
 if not exist src (
@@ -77,7 +77,7 @@ call gn.bat gen %OUTPUT_DIR% --root="src" ^
   --args="is_debug=!debug! is_clang=true target_cpu=\"!arch!\" use_custom_libcxx=false rtc_libvpx_build_vp9=true enable_libaom=true rtc_include_tests=false rtc_build_examples=false rtc_build_tools=false is_component_build=false rtc_enable_protobuf=false rtc_use_h264=true ffmpeg_branding=\"Chrome\" symbol_level=0 enable_iterator_debugging=false"
 
 rem build
-ninja.exe -C %OUTPUT_DIR% :default
+ninja.exe -C %OUTPUT_DIR% :libwebrtc
 
 rem copy static library for release build
 copy "%OUTPUT_DIR%\obj\libwebrtc.dll.lib" "%ARTIFACTS_DIR%\lib"
